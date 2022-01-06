c1527e960aaae1337dd30fa415da010466a6186f
srikar kodakandla <46400867+kodakandlasrikar@users.noreply.github.com>
2022-01-06 09:07:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.functions.channels import GetFullChannelRequest
+from telethon.tl.types import InputPeerEmpty
+from tqdm import tqdm
+
+api_id=17231443  #change this to your api_id
+api_hash='fcf8061103cd0adede64dc85095' #change this to your api_hash
+
+
+messag=0
+def download_media(group, cl, name):
+    global messag
+    messages = cl.get_messages(group, limit=200000)
+    try:
+        for message in tqdm(range(messag-1,len(messages)+1)):
+            messages[message].download_media('./' + name + '/')
+            messag=message
+        print(message)    
+    except:
+        download_media(channel.full_chat, client, title)
+  
+    #print(message)
+
+
+with TelegramClient('name', api_id, api_hash) as client:
+    result = client(GetDialogsRequest(
+        offset_date=None,
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=50000,
+        hash=0,
+    ))
+
+    title = 'Chat Title'            # Title for group chat
+    for chat in result.chats:
+        print(chat)
+
+        if chat.title == title:
+            download_media(chat, client, title)
+
+    title = 'Channel title'         # Title for channel
+    channel = client(GetFullChannelRequest(title))
+    print(channel.full_chat)
+
+    download_media(channel.full_chat, client, title)
